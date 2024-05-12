077edbe529914ce519a1be9293c1ce79db6fc3df
Salma <salmaabdijama@gmail.com>
2024-05-12 12:18:59 -0400

    uwu

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/static";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
