0210202e360d5ab75f8e30fc97136812a10d1a30
Salma <salmaabdijama@gmail.com>
2024-05-12 12:16:41 -0400

    uwu

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,16 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  image: {
+    domains: [
+      "dev-to-uploads.s3.amazonaws.com",
+      "https://dev-to-uploads.s3.amazonaws.com/",
+      "https://dev-to-uploads.s3.amazonaws.com/uploads/articles/psxvdn3z7a17rof1xn0j.png",
+      "og.anit.dev",
+    ],
+  },
+  site: "https://salmaj.me/",
+  integrations: [tailwind(), sitemap(), mdx(), prefetch()],
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
